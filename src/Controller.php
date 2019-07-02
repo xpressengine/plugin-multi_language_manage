c9c31e104545de9b63bff913ff671cac739df7ae
sirwoongke <fzwoongbin@naver.com>
2019-07-02 20:00:09 +0900

    make plugin

---
+++ src/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Xpressengine\Plugins\MultiLanguageManage;
+
+use XeFrontend;
+use XePresenter;
+use App\Http\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    public function index()
+    {
+        $title = 'MultiLanguageManage plugin';
+
+        // set browser title
+        XeFrontend::title($title);
+
+        // load css file
+        XeFrontend::css(Plugin::asset('assets/style.css'))->load();
+
+        // output
+        return XePresenter::make('multi_language_manage::views.index', ['title' => $title]);
+    }
+}
